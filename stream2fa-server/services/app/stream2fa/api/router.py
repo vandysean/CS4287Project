0ffe898ba8ae2e81511b7f10b5776f9112f592a9
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2021-11-06 12:54:55 -0500

    fix router

--- stream2fa-server/services/app/stream2fa/api/router.py
+++ stream2fa-server/services/app/stream2fa/api/router.py
@@ -10,7 +10,7 @@ from stream2fa.api.routes import (
 
 router = APIRouter()
 
-router.include_router(base.router, tags=["base"], prefix="/")
+router.include_router(base.router, tags=["base"], prefix="")
 router.include_router(test_routes.router, tags=["test"], prefix="/test")
 router.include_router(authorization.router, tags=["auth"], prefix="/auth")
 router.include_router(registration.router, tags=["register"], prefix="/register")
