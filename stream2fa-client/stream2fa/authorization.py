1ab34a7f2524b95843c26f41952615e4ae9fc868
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2021-11-07 17:45:26 -0600

    add client api

---
+++ stream2fa-client/stream2fa/authorization.py
@@ -0,0 +1,26 @@
+from stream2fa.endpoints import (
+    USER_PWD_AUTH_ENDPOINT,
+    IMMEDIATE_REDIRECT_ENDPOINT,
+    GET_AUTH_STREAM_TEMPLATE_ENDPOINT
+)
+
+import requests
+import json
+
+
+def authorize_user(*, username: str, password: str, app: str, success_url: str, failure_url: str) -> str:
+    hashed_password = password  # do whatever hashing/encryption here
+    
+    pwd_auth_data = {'username': username, 'password': hashed_password, 'app': app}
+    pwd_auth_response = requests.post(USER_PWD_AUTH_ENDPOINT, data=json.dumps(pwd_auth_data)).json()
+        
+    if pwd_auth_response['status'] == 'success':
+        stream_auth_data = {'username': username, 'app': app, 'success_url': success_url, 'failure_url': failure_url}
+        stream_auth_response = requests.post(GET_AUTH_STREAM_TEMPLATE_ENDPOINT, data=json.dumps(stream_auth_data))
+        
+        return stream_auth_response.text
+    else:
+        redirect_data = {'url': failure_url}
+        redirect_response = requests.post(IMMEDIATE_REDIRECT_ENDPOINT, data=json.dumps(redirect_data)) 
+        
+        return redirect_response.text
