bdba1d366b1a862ad604f0b2d123a9cc6620a607
vandysean <sean.j.tuttle@vanderbilt.com>
2021-11-08 05:15:48 +0000

    update decode_base64_image

--- stream2fa-server/services/app/stream2fa/common/functions.py
+++ stream2fa-server/services/app/stream2fa/common/functions.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import base64
 
-async def decode_base64_image(uri) -> np.ndarray:
+async def decode_base64_image(uri):
    img_encoded_b64 = uri.split(',')[1]
    img_np_arr = np.frombuffer(base64.b64decode(img_encoded_b64), np.uint8)
-   
-   return cv2.imdecode(img_np_arr, cv2.IMREAD_COLOR)
+   img = cv2.imdecode(img_np_arr, cv2.IMREAD_COLOR)
+
+   return img
