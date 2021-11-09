99642638f1c5f3b91439bc94552a25b1da299d46
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2021-11-09 10:29:09 -0600

    add setup script for client library

---
+++ stream2fa-client/stream2fa/setup.py
@@ -0,0 +1,12 @@
+import pkg_resources
+import subprocess
+import sys
+
+REQUIRED = {'requests'}
+
+installed = {pkg.key for pkg in pkg_resources.working_set}
+missing = REQUIRED - installed
+
+if missing:
+    python = sys.executable
+    subprocess.check_call([python, '-m', 'pip', 'install', *missing], stdout=subprocess.DEVNULL)
