0100e210dd736a6f4a172552c7afd07512abe0f8
vandysean <sean.j.tuttle@vanderbilt.edu>
2021-11-15 04:24:31 +0000

    remove app from models

--- stream2fa-server/services/app/stream2fa/api/models.py
+++ stream2fa-server/services/app/stream2fa/api/models.py
@@ -1,28 +1,21 @@
 from pydantic import BaseModel
 
-class TestMessage(BaseModel):
-    text: str
-    author: str
-
 class StreamFrame(BaseModel):
     uri: str
     username: str
-    app: str
     
 class UserInfo(BaseModel):
     username: str
     password: str
-    app: str
     
 class DeleteUserInfo(BaseModel):
     username: str
-    app: str
     
 class StreamTemplateInfo(BaseModel):
     username: str
-    app: str
     success_url: str
     failure_url: str
     
 class RedirectTemplateInfo(BaseModel):
     url: str
+
