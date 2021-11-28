7c5ad32ad185b40b042bf09fc6d2dbff2f387f3d
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2021-11-28 12:09:20 -0600

    remove testapp shards

--- stream2fa-server/services/app/stream2fa/common/constants.py
+++ stream2fa-server/services/app/stream2fa/common/constants.py
@@ -5,5 +5,3 @@ MAX_NUM_ENCODINGS_SAVED = 15
 DB_URI = "mongodb+srv://admin:IpjsuXtcjy0Bmz6G@cluster0.6m2dg.mongodb.net/Cluster0?retryWrites=true&w=majority"
 
 HOST_NAME = "stream2fa.com"
-
-SECRET_KEY = secrets.token_hex(32)
--- stream2fa-server/services/app/stream2fa/common/objects.py
+++ stream2fa-server/services/app/stream2fa/common/objects.py
@@ -6,6 +6,4 @@ from stream2fa.common.constants import DB_URI
 
 templates = Jinja2Templates(directory="stream2fa/templates")
 
-# client = pymongo.MongoClient(DB_URI, ssl_cert_reqs=ssl.CERT_NONE)
-# db = client.user_login_system
 db = pymongo.MongoClient(DB_URI, ssl_cert_reqs=ssl.CERT_NONE).user_login_system
