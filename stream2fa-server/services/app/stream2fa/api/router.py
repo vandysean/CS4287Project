063bdd7951366e3a70bd216dbd81651bafac3f75
tuttlsj1 <sean.j.tuttle@vanderbilt.edu>
2021-11-28 12:02:45 -0600

    remove testapp shards

--- stream2fa-server/services/app/stream2fa/api/router.py
+++ stream2fa-server/services/app/stream2fa/api/router.py
@@ -7,7 +7,6 @@ from stream2fa.api.routes import (
     downloads,
     redirect,
     deletion,
-    testapp
 )
 
 router = APIRouter()
@@ -18,4 +17,3 @@ router.include_router(registration.router, tags=["register"], prefix="/register"
 router.include_router(downloads.router, tags=["download"], prefix="/download")
 router.include_router(redirect.router, tags=["redirect"], prefix="/redirect")
 router.include_router(deletion.router, tags=["delete"], prefix="/delete")
-router.include_router(deletion.router, tags=["testapp"], prefix="/testapp")
